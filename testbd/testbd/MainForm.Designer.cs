f514e7e51b6abf239b932401ebc27300e4c9ad8e
IvanSemenovTeacher <40857580+IvanSemenovTeacher@users.noreply.github.com>
2019-12-10 08:26:02 +0300

    подключение к базе данных

---
+++ testbd/testbd/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Преподаватель
+ * Date: 05.12.2019
+ * Time: 14:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace testbd
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(184, 60);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(338, 257);
+			this.dataGridView1.TabIndex = 1;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(12, 60);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(120, 251);
+			this.listBox1.TabIndex = 2;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1SelectedIndexChanged);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(117, 14);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(183, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 356);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(510, 142);
+			this.textBox2.TabIndex = 4;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 504);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(549, 586);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "testbd";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ testbd/testbd/MainForm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Преподаватель
+ * Date: 05.12.2019
+ * Time: 14:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Net.Sockets;
+using System.Data;
+namespace testbd
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		MySqlConnection connection;
+		void Button1Click(object sender, EventArgs e)
+		{
+			string tablename = textBox1.Text;
+			string connect ="server=192.168.8.100;database="+tablename+";port=3306;user id=root;password=123Qq_123;SSL mode = none";
+			connection = new MySqlConnection(connect);
+			try
+			{
+				connection.Open();
+				button1.BackColor = Color.GreenYellow;
+				
+				MySqlCommand com = new MySqlCommand("show tables;",connection);
+				MySqlDataReader reader = com.ExecuteReader();
+				while(reader.Read())
+				{
+					listBox1.Items.Add(reader.GetValue(0).ToString());
+				}
+				reader.Close();
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+		
+		void ListBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string tablename = listBox1.SelectedItem.ToString();
+			MySqlCommand com = new MySqlCommand("select * from "+tablename,connection);
+			DataTable table = new DataTable();
+			table.Load(com.ExecuteReader());
+			dataGridView1.DataSource = table;
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+string sql = textBox2.Text;
+MySqlCommand com = new MySqlCommand(sql,connection);
+
+DataTable table = new DataTable();
+			table.Load(com.ExecuteReader());
+			dataGridView1.DataSource = table;
+		}
+	}
+}
